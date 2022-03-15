5ee0a046e02243deef864dd983ae43aaeccdc658
fernando.moreiradantas@gmail.com <fernando.moreiradantas@gmail.com>
2022-03-14 21:48:58 -0300

    Componentes

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Route,Switch,HashRouter} from "react-router-dom";
 import CadastroUsuario from "../views/cadastroUsuarios";
+import Home from "../views/home";
 import Login from "../views/login";
 
 function Rotas(){
     return(
         <HashRouter>
             <Switch>
+                <Route path={"/home"} component={Home}/>
                 <Route path={"/login"} component={Login}/>
                 <Route path={"/cadastro-usuarios"} component={CadastroUsuario}/>
             </Switch>
