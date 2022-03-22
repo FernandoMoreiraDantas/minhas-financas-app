d0301f09aea5da7d89af782df066a17185ab45ca
fernando.moreiradantas@gmail.com <fernando.moreiradantas@gmail.com>
2022-03-22 18:33:05 -0300

    Implementaando o salver dados de usuario

--- src/views/cadastroUsuarios.js
+++ src/views/cadastroUsuarios.js
@@ -2,81 +2,102 @@ import React from "react";
 
 import Card from "../components/card";
 import FormGroup from "../components/form-groups";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
-class CadastroUsuario extends React.Component{
-    state={
-        nome:'',
-        email:'',
-        senha:'',
-        senhaRepeticao:''
+import UsuarioService from "../app/service/usuarioService";
+import { mensagemErro, mensagemSucesso } from "../components/toastr";
+
+class CadastroUsuario extends React.Component {
+    state = {
+        nome: '',
+        email: '',
+        senha: '',
+        senhaRepeticao: ''
 
     }
-    cadastrar = () =>{
-        console.log(this.state)
-       
+
+    constructor() {
+        super()
+        this.service = new UsuarioService();
     }
-    cancelar = () =>{
+
+    cadastrar = () => {
+        const usuario = {
+            nome: this.state.nome,
+            email: this.state.email,
+            senha: this.state.senha
+        }
+        this.service.salvar(usuario)
+            .then(response => {
+                mensagemSucesso('Usuário cadastrado com Sucesso.')
+                this.props.history.push('login')
+            })
+            .catch(error => {
+                mensagemErro(error.response.data)
+            })
+
+    }
+    cancelar = () => {
         this.props.history.push('login')
-       
+
     }
 
-    render(){
+    render() {
         return (
-                <Card title="Cadastro de Usuário">
+            <Card title="Cadastro de Usuário">
                 <div className="row">
-                                    <div className="col-lg-12"> 
-                                            <div className="bs-Component">
-                                                <fieldset>
-                                                <FormGroup label="Nome:*" htmlFor="inputNome">
-                                                        <input type="text" 
-                                                            value={this.state.email}
-                                                            className="form-control"
-                                                            id="inputNome"
-                                                            placeholder="Digite o nome"
-                                                            name="nome"
-                                                            onChange={e => this.setState({nome:e.target.value})} />
+                    <div className="col-lg-12">
+                        <div className="bs-Component">
+                            <fieldset>
+                                <FormGroup label="Nome:*" htmlFor="inputNome">
+                                    <input type="text"
+                                        value={this.state.nome}
+                                        className="form-control"
+                                        id="inputNome"
+                                        placeholder="Digite o nome"
+                                        name="nome"
+                                        onChange={e => this.setState({ nome: e.target.value })} />
 
-                                                    </FormGroup>
-                                                    <br/>
-                                                    <FormGroup label="Email:*" htmlFor="inputEmail">
-                                                        <input type="text" 
-                                                        value={this.state.email}
-                                                               className="form-control"
-                                                               id="inputEmail"
-                                                               aria-describedby="emailHelp"
-                                                               placeholder="Digite o e-mail"
-                                                               onChange={e => this.setState({email:e.target.value})} />
+                                </FormGroup>
+                                <br />
+                                <FormGroup label="Email:*" htmlFor="inputEmail">
+                                    <input type="text"
+                                        value={this.state.email}
+                                        className="form-control"
+                                        id="inputEmail"
+                                        aria-describedby="emailHelp"
+                                        placeholder="Digite o e-mail"
+                                        onChange={e => this.setState({ email: e.target.value })} />
 
-                                                    </FormGroup>
-                                                    <br/>
-                                                    <FormGroup label="Senha:*" htmlFor="inputSenha">
-                                                        <input type="password" 
-                                                               value={this.senha}
-                                                               className="form-control"
-                                                               id="inputSenha"
-                                                               placeholder="Password"
-                                                               onChange={e => this.setState({senha:e.target.value})}/>
+                                </FormGroup>
+                                <br />
+                                <FormGroup label="Senha:*" htmlFor="inputSenha">
+                                    <input type="password"
+                                        value={this.senha}
+                                        className="form-control"
+                                        id="inputSenha"
+                                        placeholder="Password"
+                                        onChange={e => this.setState({ senha: e.target.value })} />
 
-                                                    </FormGroup>
-                                                    <br/>
-                                                    <FormGroup label="Confirme a Senha:*" htmlFor="inputSenhaRepetida">
-                                                        <input type="password" 
-                                                               value={this.senha}
-                                                               className="form-control"
-                                                               id="inputSenhaRepetida"
-                                                               placeholder="Confirme a senha"
-                                                               onChange={e => this.setState({senhaRepeticao:e.target.value})}/> 
+                                </FormGroup>
+                                <br />
+                                <FormGroup label="Confirme a Senha:*" htmlFor="inputSenhaRepetida">
+                                    <input type="password"
+                                        value={this.senha}
+                                        className="form-control"
+                                        id="inputSenhaRepetida"
+                                        placeholder="Confirme a senha"
+                                        onChange={e => this.setState({ senhaRepeticao: e.target.value })} />
 
-                                                    </FormGroup>
-                                                    <br/>
-                                                    <button onClick={this.cadastrar} className="btn btn-success">Salvar</button>
-                                                    <button onClick={this.cancelar} className="btn btn-danger">Cancelar</button>
-                                                </fieldset>
-                                            </div>
-                                    </div>
-                                </div>
-                </Card>
+                                </FormGroup>
+                                <br />
+                                <button onClick={this.cadastrar} className="btn btn-success">Salvar</button>
+                                <button onClick={this.cancelar} className="btn btn-danger">Cancelar</button>
+                            </fieldset>
+                        </div>
+                    </div>
+                </div>
+            </Card>
         )
     }
 }
